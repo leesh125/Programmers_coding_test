1bcab3895f595d3140fe6dabe053a754cbc345c8
leesh125 <chelsea12345@naver.com>
2022-10-29 18:47:15 +0900

    Lv2. 연습문제 - 할인 행사

--- lv2/Python3/할인행사.py
+++ lv2/Python3/할인행사.py
@@ -5,26 +5,32 @@ def solution(want, number, discount):
     
     for i in range(len(want)):
         dic[want[i]] = number[i]
-    print(dic)
-#     temp_total = total
-#     for i in range(total):
-#         if discount[i] in dic:
-#             if dic[discount[i]] > 0:
-#                 dic[discount[i]] -= 1
-#                 temp_total -= 1
-#     if temp_total == 0: answer +=1
+    temp_total = total
+    for i in range(total):
+        if discount[i] in dic:
+            if dic[discount[i]] > 0:
+                dic[discount[i]] -= 1
+                temp_total -= 1
+            else:
+                dic[discount[i]] -= 1
+    if temp_total == 0: answer +=1
     
-#     left_idx = 1; right_idx = total
-#     while right_idx < len(discount):
-#         if discount[left_idx-1] in dic:
-#             temp_total += 1
-#             discount[left_idx-1] += 1
-#         if discount[left_idx] in dic:
-#             temp_total -= 1
-#             discount[left_idx] -= 1
-#         if discount[right_idx] in dic:
-#             temp_total -= 1
-#             discount[right_idx] -= 1
-#         if temp_total == 0: answer +=1
-    
+    left_idx = 1; right_idx = total
+    while right_idx < len(discount):
+        if discount[left_idx-1] in dic:
+            if dic[discount[left_idx-1]] < 0:
+                pass
+            else:
+                temp_total += 1
+            dic[discount[left_idx-1]] += 1
+        if discount[right_idx] in dic:
+            if dic[discount[right_idx]] > 0:
+                temp_total -= 1
+            else:
+                pass
+            dic[discount[right_idx]] -= 1
+        if temp_total == 0: answer +=1
+        
+        left_idx += 1; right_idx += 1
+        
     return answer
